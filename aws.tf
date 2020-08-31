238aa0a15cb73897eeac80ec07c1415e12ecd098
Ubuntu <ubuntu@ip-172-31-16-30.us-east-2.compute.internal>
2020-08-31 02:24:43 +0000

    Added terraform files

---
+++ aws.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  access_key = "AKIAJKLCHZXSKKHJGW5A"
+  secret_key = "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"  
+  region     = "us-east-2"
+}
---
+++ create_ec2.tf
@@ -0,0 +1,42 @@
+resource "aws_instance" "myFirstInstance" {
+  ami           = "ami-06817f01dcc7f30be"
+  count=1
+  key_name = "My202008NEWKEY"
+  instance_type = "t2.micro"
+  security_groups= [ "security_jenkins_port"]
+  tags= {
+    Name = "jenkins_instance"
+  }
+}
+
+resource "aws_security_group" "security_jenkins_port" {
+  name        = "security_jenkins_port"
+  description = "security group for jenkins"
+
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+ # outbound from jenkis server
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags= {
+    Name = "security_jenkins_port"
+  }
+}
+
